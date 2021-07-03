3095eedec5bf5938ec143ea5026261d893f55795
eilmadc <eilmadc@gmail.com>
2021-07-04 01:18:04 +0200

    Create project

---
+++ src/environments/firebaseconfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCZQ-Otozgq4NUFnep-_5z8qT06BtfNggc",
+    authDomain: "cf-ionicfirebase0.firebaseapp.com",
+    projectId: "cf-ionicfirebase0",
+    storageBucket: "cf-ionicfirebase0.appspot.com",
+    messagingSenderId: "197535500907",
+    appId: "1:197535500907:web:92a9de4af15cb6a249bf2e",
+    measurementId: "G-LNB4DEKCJJ"
+  };
